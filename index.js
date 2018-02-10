e2578485f0b24bb6c4348eeb0325b670fbf49ee4
Qingyun Xiu <qingyunx@msn.com>
2018-02-10 16:59:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-﻿var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
